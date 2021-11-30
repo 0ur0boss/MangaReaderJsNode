b6218c92d4d057763dea69d69da04995e85684f5
0ur0boss <enzo.duret@yahoo.fr>
2021-11-30 16:57:41 +0100

    ajoue de la search bar et deplacement des cards

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import styles from "./SearchBar.module.css";
 import { cards } from "../../components/data/cards";
+import Card from "../../components/Card";
 
 
 const SearchBar = () => {
@@ -9,17 +10,28 @@ const SearchBar = () => {
     const searchText = (event) =>{
         setFilter(event.target.value);
     }
-    console.warn(filter)
+    let dataSearch = cards.filter(item => {
+        return Object.keys(item).some(key =>
+            item[key].toString().toLowerCase().includes(filter.toString().toLowerCase())
+    )});
+    
     return (
         <section>
             <div>
+                <div>
                 <label />
                 <input 
                 type="text"
                 value={filter}
-
-                
+                onChange={searchText.bind(this)}              
                 />
+                </div>
+                {/* On affiche les cards en function de la search bar (avec dataSearch) */}
+                <div>
+                {dataSearch.map(({ img, title, btn, description }) => (
+                <Card img={img} title={title} btn={btn} description={description} />
+                ))}
+                </div>
             </div>
         </section>
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 // import { Link } from 'react-router-dom'
 import styles from "./Home.module.css";
-import { cards } from "../../components/data/cards";
-import Card from "../../components/Card";
+
 import Slider from "components/Slider";
 import Search from "components/SearchBar";
 
@@ -10,18 +9,13 @@ const Home = () => {
   return (
     <div>
       <div>
-        <Search/>
-      </div>
-      <div>
         <Slider />
       </div>
 
       {/* le container qui a toutes les cards */}
       <div className={styles.wrapper}>
-        {/* une card avec les differents options */}
-        {cards.map(({ img, title, btn, description }) => (
-          <Card img={img} title={title} btn={btn} description={description} />
-          ))}
+        {/* La search et toutes les cartes */}
+        <Search/>
       </div>
     </div>
   );
