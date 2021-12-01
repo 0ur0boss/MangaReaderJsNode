4b2aff1aabff59e8482b67059964bd14bccb4a3f
0ur0boss <enzo.duret@yahoo.fr>
2021-12-01 15:51:34 +0100

    pull

--- src/pages/Webtoon/Webtoon.js
+++ src/pages/Webtoon/Webtoon.js
@@ -1,11 +1,37 @@
-// import styles from './Webtoon.module.css';
-import React from 'react' 
+import React, { useContext } from 'react' 
+import BtnFav from "components/BtnFav";
+import { Link } from "react-router-dom";
+import AppContext from "../../contexts/AppContext";
 
 
-const Webtoon = () => {
+const Webtoon = ({ manga, favs, handleFav }) => {
+   const { mangas, favs, toggleFav } = useContext(AppContext);
+   const { img, title, btn, description, id } = manga;
 
+   return (
+      <div>
+        <div>
+          <img src={img} alt="" />
+          <h2> {title}</h2>
+          <p>{description}</p>
+        </div>
+        {/* <button className={styles.card_btn}>{Btn}</button> */}
+        <Link to="/manga-description">
+          <button
+            type="button"
+          >
+            {btn}
+          </button>
+        </Link>
   
-   return "coucou"
+        <BtnFav
+          id={id}
+          isFaved={favs.includes(id)}
+          onClick={() => handleFav(id)}
+        />
+      </div>
+    );
+
 }
 
 export default Webtoon
